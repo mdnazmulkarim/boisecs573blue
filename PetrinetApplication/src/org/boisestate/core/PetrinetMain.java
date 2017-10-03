ea74c86b184b912a2444891084422fa8e2ed631c
Md Nazmul Karim <Md Nazmul Karim@LAPTOP-7RROLCRR.hitronhub.home>
2017-10-03 11:16:52 -0600

    Place Class

--- PetrinetApplication/src/org/boisestate/core/PetrinetMain.java
+++ PetrinetApplication/src/org/boisestate/core/PetrinetMain.java
@@ -3,7 +3,9 @@ package org.boisestate.core;
 public class PetrinetMain {
 
 	public static void main(String[] args) {
-		System.out.println("Testing Petrinet.");
+		System.out.println("This is the main program of petrinet");
+		
+	
 
 	}
 
