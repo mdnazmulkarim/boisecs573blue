4d969e89d3fdcfab5850ed450b44042ab49c1886
MBhowmick17 <mousumebhowmick@u.boisestate.edu>
2017-10-03 18:57:35 -0600

    Update of MainPanel

--- PetrinetApplication/src/org/boisestate/core/PetrinetMain.java
+++ PetrinetApplication/src/org/boisestate/core/PetrinetMain.java
@@ -8,7 +8,7 @@ public class PetrinetMain {
 
 	public static void main(String[] args) {
 		System.out.println("This is the main program of petrinet");
-		
+		System.out.println("This is main class");
 		MainPanel mainPanel = new MainPanel();
 		mainPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainPanel.setVisible(true);
