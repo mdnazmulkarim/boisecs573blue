2f0f35a62eda4cb0a0965ac5e4a383714c391b25
MBhowmick17 <mousumebhowmick@u.boisestate.edu>
2017-10-09 19:57:20 -0600

    Place and transition drawing....

--- PetrinetApplication/src/org/boisestate/graphics/PlaceGuiItem.java
+++ PetrinetApplication/src/org/boisestate/graphics/PlaceGuiItem.java
@@ -4,27 +4,39 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class PlaceGuiItem {
-	protected int x,y;
-	
-	protected int outerRadius = 50;
-	protected int innerRadius = 48;
-	
-	PlaceGuiItem(){
-		
-	}
-	
-	public void setCoordinate(double p,double q) {
-		x = (int)p;
-		y = (int)q;
-		System.out.println(x + " " + y);
-	}
-	public void paintComponent (Graphics canvas) {
-		
-		canvas.setColor(Color.BLACK);
-		canvas.fillOval(x-outerRadius,y-outerRadius,outerRadius,outerRadius);
-		
-		canvas.setColor(Color.WHITE);
-		canvas.fillOval(x-innerRadius,y-innerRadius,innerRadius,innerRadius);
-	}
+	private int xPos = 50;
+    private int yPos = 50;
+    private int width = 20;
+    private int height = 20;
+    
+    public void setX(int xPos){ 
+        this.xPos = xPos;
+    }
 
+    public int getX(){
+        return xPos;
+    }
+
+    public void setY(int yPos){
+        this.yPos = yPos;
+    }
+
+    public int getY(){
+        return yPos;
+    }
+
+    public int getWidth(){
+        return width;
+    } 
+
+    public int getHeight(){
+        return height;
+    }
+
+    public void paintPlace(Graphics g){
+        g.setColor(Color.white);
+        g.fillOval(xPos, yPos, width, height);
+        g.setColor(Color.BLACK);
+        g.drawOval(xPos,yPos,width,height);  
+    }
 }
--- PetrinetApplication/src/org/boisestate/graphics/TransitionGuiItem.java
+++ PetrinetApplication/src/org/boisestate/graphics/TransitionGuiItem.java
@@ -1,5 +1,42 @@
 package org.boisestate.graphics;
 
-public class TransitionGuiItem {
+import java.awt.Color;
+import java.awt.Graphics;
 
+public class TransitionGuiItem {
+	private int xPos = 0;
+    private int yPos = 0;
+    private int width = 20;
+    private int height = 40;
+    
+    public void setX(int xPos){ 
+        this.xPos = xPos;
+    }
+
+    public int getX(){
+        return xPos;
+    }
+
+    public void setY(int yPos){
+        this.yPos = yPos;
+    }
+
+    public int getY(){
+        return yPos;
+    }
+
+    public int getWidth(){
+        return width;
+    } 
+
+    public int getHeight(){
+        return height;
+    }
+
+    public void paintTransition(Graphics g){
+        g.setColor(Color.GRAY);
+        g.fillRect(xPos, yPos, width, height);
+        g.setColor(Color.BLACK);
+        g.drawRect(xPos,yPos,width,height);  
+    }
 }
